98e5de3ebd9611b55cc0fb2c610accd45641dfcd
Mariana <165081625+myslium@users.noreply.github.com>
2024-10-12 01:17:15 -0300

    front

--- frontend/src/pages/entrar/index.jsx
+++ frontend/src/pages/entrar/index.jsx
@@ -1,8 +1,25 @@
 import './index.scss'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 
 export default function Home(){
+
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const Navegation = useNavigate()
+
+    async function acessar() {
+        const usuario = {
+            "nome": nome,
+            "senha": senha
+        }
+
+        const url = `http://localhost:2007/entrar`
+        let resposta = await axios.post(url, usuario)
+    }
 
     return (
         <div className='pagina-login'>
